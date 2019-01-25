8e37f2873a9402867498ae6f2d3786d520350b91
kalaibenjamin <46839389+kalaibenjamin@users.noreply.github.com>
2019-01-25 14:48:34 +0530

    Create largest number.py

---
+++ largest number.py
@@ -0,0 +1,10 @@
+ no=10
+ no=23
+ no=5
+ if (no1 >= no2) and (no1 >= no3):
+   largest = no1
+elif (no2 >= no1) and (no2 >= no3):
+   largest = no2
+else:
+   largest = no3
+print(the largest number is ,"no1","no2","no3")
