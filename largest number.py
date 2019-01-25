3d41af6154c4ce2bfa1f1baee9cf3ccf58dce712
kalaibenjamin <46839389+kalaibenjamin@users.noreply.github.com>
2019-01-25 15:08:49 +0530

    Update largest number.py

--- largest number.py
+++ largest number.py
@@ -1,7 +1,7 @@
 no=input("enter a three no")
- if (no1 >= no2) and (no1 >= no3):
+ if (no1>=no2) and (no1>=no3):
    largest = no1
-elif (no2 >= no1) and (no2 >= no3):
+elif(no2>=no1) and (no2>=no3):
    largest = no2
 else:
    largest = no3
