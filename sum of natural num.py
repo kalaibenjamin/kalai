ea441f6e5ace7ef82adfdc0c8407846d956766bd
kalaibenjamin <46839389+kalaibenjamin@users.noreply.github.com>
2019-02-11 14:11:23 +0530

    Update sum of natural num.py

--- sum of natural num.py
+++ sum of natural num.py
@@ -1,10 +1,10 @@
-num = int(input("Enter the value of n:"))
-n = num
+number = int(input("Enter the value of n:"))
+n = number
 sum = 0
-if num <= 0: 
+if number <= 0: 
    print("Enter a whole positive number") 
 else: 
-   while num > 0:
-      sum = sum + num;
-      num = num - 1; 
+   while number > 0:
+      sum = sum + number;
+      number = number- 1; 
 print("Sum of first", n,"natural numbers is:",sum)
