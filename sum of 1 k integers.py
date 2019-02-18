e2f1c15ae8ce6d9724b29b209cb34532689edcb4
kalaibenjamin <46839389+kalaibenjamin@users.noreply.github.com>
2019-02-18 14:56:53 +0530

    Create sum of 1 k integers.py

---
+++ sum of 1 k integers.py
@@ -0,0 +1,21 @@
+class listnode(object):
+def_init_(self,y):
+self.value=y
+self.next=name
+class add two number(self,11,12):
+carry=o
+head=curr=listnode(0)
+while 11 or 12
+value=carry
+if 11:
+value +=11.value
+11=11.value
+if 12:
+value +=12.value
+12=12.next
+curr.next=listnode(value%10)
+curr+curr.next
+carry=value/10
+if carry>0:
+carry.next=listnode(carry)
+return head.next
